a53a4ddd4ed0ed09de566be35c3312290bcc528a
Priyanka Gundla <gpriyanka1621@gmail.com>
2026-06-10 15:00:31 +0530

    authenticated

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -11,9 +11,9 @@ import ResetPassword from "../pages/auth/ResetPassword";
 import Profile from "../pages/ProfileInfo";
 
 
-//const isAuthenticated = () => !!localStorage.getItem("token");
+const isAuthenticated = () => !!localStorage.getItem("accessToken");
 
-const isAuthenticated = () => true;
+// const isAuthenticated = () => true;
 
 function AppRouter() {
   return (
@@ -22,14 +22,15 @@ function AppRouter() {
         {/* Public Routes (NO Sidebar / Header) */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/email-confirmation" element={<PasswordResetConfirmation />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+                   <Route path="/forgot-password" element={<ForgotPassword />} />
+
 
         {/* Protected Routes (WITH Sidebar & Header) */}
         <Route
           element={isAuthenticated() ? <Layout /> : <Navigate to="/login" />}
         >
+        <Route path="/email-confirmation" element={<PasswordResetConfirmation />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/" element={<Dashboard />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/expenses" element={<ExpensePage />} />
